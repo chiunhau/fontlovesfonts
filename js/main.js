987c25afac6be339e0608051b2731e0a8f45fbae
Chiunhau <chiunhau9@gmail.com>
2015-06-23 12:05:39 +0800

    change to id

--- js/main.js
+++ js/main.js
@@ -1,15 +1,107 @@
-var data = [ {id: 1, typeface: "Futura"}, {id: 2, typeface: "Courier"}, {id: 3, typeface: "Hiragino"}, {id: 4, typeface: "Arial"}, {id: 5, typeface: "Avenir"}, {id: 6, typeface: "Rockwell"} ];
+var data = [ 
+	{id: 1, typeface: "Futura"}, 
+	{id: 2, typeface: "Courier"}, 
+	{id: 3, typeface: "Hiragino"}, 
+	{id: 4, typeface: "Arial"}, 
+	{id: 5, typeface: "Avenir"}, 
+	{id: 6, typeface: "Rockwell"}, 
+	{id: 7, typeface: "Courier Neue"}, 
+	{id: 8, typeface: "Helvetica"}, 
+	{id: 9, typeface: "Trajan"}, 
+	{id: 10, typeface: "Glober"}
+];
+
+var Container = React.createClass({displayName: "Container",
+  render: function() {
+  	return (
+			React.createElement("div", {className: "container"}, 
+				React.createElement(FontBox, null), 
+				React.createElement(FontBar, {data: this.props.data})
+			)
+  	);
+  }
+});
+
+var FontBox = React.createClass({displayName: "FontBox",
+  render: function() {
+	  return (
+			React.createElement("div", {className: "font-box"}, 
+				React.createElement(FontSquareLeft, null), 
+				React.createElement(FontSquareRight, null)
+			)
+  	);
+     
+  }
+});
+
+var FontSquareLeft = React.createClass({displayName: "FontSquareLeft",
+  render: function() {
+  	return (
+			React.createElement("div", {className: "square square-left"}, 
+				"呦"
+			)
+  	);
+  }
+});
+
+var FontSquareRight = React.createClass({displayName: "FontSquareRight",
+  render: function() {
+  	return (
+			React.createElement("div", {className: "square square-right"}, 
+				"嘿"
+			)
+  	);
+  }
+});
+
+var FontBar = React.createClass({displayName: "FontBar",
+  render: function() {
+    return (
+    	React.createElement("div", {className: "font-bar"}, 
+	    	React.createElement(ArrowLeft, null), 
+	    	React.createElement(FontsList, {data: this.props.data}), 
+	    	React.createElement(BarMask, null), 
+	    	React.createElement(ArrowRight, null)
+	    )
+    );
+  }
+});
+
+var ArrowLeft = React.createClass({displayName: "ArrowLeft",
+  render: function() {
+    return (
+    	React.createElement("div", {className: "arrow-left"})
+  	);
+  }
+});
+
+var ArrowRight = React.createClass({displayName: "ArrowRight",
+  render: function() {
+    return (
+    	React.createElement("div", {className: "arrow-right"})
+  	);
+  }
+});
+
+var BarMask = React.createClass({displayName: "BarMask",
+  render: function() {
+  	return (
+  		React.createElement("div", {className: "bar-mask"})
+  	);
+     
+  }
+});
 
 var FontsList = React.createClass({displayName: "FontsList",
   render: function() {
   	var fontNodes = this.props.data.map(function(font){
   		return (
-  			React.createElement(Font, {typeface: font.typeface}
+  			React.createElement(Font, {typeface: font.typeface, id: font.id}
   			)
   		);
   	});
   	return (
-  		React.createElement("div", {className: "fontList"}, 
+  		React.createElement("div", {className: "font-list"}, 
   			React.createElement("ul", null, 
   				fontNodes
   			)
@@ -27,13 +119,13 @@ var Font = React.createClass({displayName: "Font",
   render: function() {
     return (
     	React.createElement("li", {className: "font", id: this.props.typeface, onClick: this.handleClick}, 
-    		"嘿"
+    		this.props.id
     	)
     );
   }
 });
 
 React.render(
-	React.createElement(FontsList, {data: data}),
+	React.createElement(Container, {data: data}),
 	document.getElementById("content")
 );
--- jsx/main.js
+++ jsx/main.js
@@ -1,11 +1,68 @@
-var data = [ {id: 1, typeface: "Futura"}, {id: 2, typeface: "Courier"}, {id: 3, typeface: "Hiragino"}, {id: 4, typeface: "Arial"}, {id: 5, typeface: "Avenir"}, {id: 6, typeface: "Rockwell"} ];
+var data = [ 
+	{id: 1, typeface: "Futura"}, 
+	{id: 2, typeface: "Courier"}, 
+	{id: 3, typeface: "Hiragino"}, 
+	{id: 4, typeface: "Arial"}, 
+	{id: 5, typeface: "Avenir"}, 
+	{id: 6, typeface: "Rockwell"}, 
+	{id: 7, typeface: "Courier Neue"}, 
+	{id: 8, typeface: "Helvetica"}, 
+	{id: 9, typeface: "Trajan"}, 
+	{id: 10, typeface: "Glober"}
+];
+
+var Container = React.createClass({
+  render: function() {
+  	return (
+			<div className="container">
+				<FontBox />
+				<FontBar data={this.props.data} />
+			</div>
+  	);
+  }
+});
+
+var FontBox = React.createClass({
+  render: function() {
+	  return (
+			<div className="font-box">
+				<FontSquareLeft />
+				<FontSquareRight />
+			</div>
+  	);
+     
+  }
+});
+
+var FontSquareLeft = React.createClass({
+  render: function() {
+  	return (
+			<div className="square square-left">
+				呦
+			</div>
+  	);
+  }
+});
+
+var FontSquareRight = React.createClass({
+  render: function() {
+  	return (
+			<div className="square square-right">
+				嘿
+			</div>
+  	);
+  }
+});
 
 var FontBar = React.createClass({
   render: function() {
     return (
-    	<ArrowLeft />
-    	<FontsList />
-    	<ArrowRight />
+    	<div className="font-bar">
+	    	<ArrowLeft />
+	    	<FontsList data={this.props.data}/>
+	    	<BarMask />
+	    	<ArrowRight />
+	    </div>
     );
   }
 });
@@ -26,16 +83,25 @@ var ArrowRight = React.createClass({
   }
 });
 
+var BarMask = React.createClass({
+  render: function() {
+  	return (
+  		<div className="bar-mask"></div>
+  	);
+     
+  }
+});
+
 var FontsList = React.createClass({
   render: function() {
   	var fontNodes = this.props.data.map(function(font){
   		return (
-  			<Font typeface={font.typeface}>
+  			<Font typeface={font.typeface} id={font.id}>
   			</Font>
   		);
   	});
   	return (
-  		<div className="fontList">
+  		<div className="font-list">
   			<ul>
   				{fontNodes}
   			</ul>
@@ -53,13 +119,13 @@ var Font = React.createClass({
   render: function() {
     return (
     	<li className="font" id={this.props.typeface} onClick={this.handleClick}>
-    		嘿
+    		{this.props.id}
     	</li>
     );
   }
 });
 
 React.render(
-	<FontsList data={data} />,
+	<Container data={data} />,
 	document.getElementById("content")
 );
